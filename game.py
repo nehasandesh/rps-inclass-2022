0c691e3ba0d89788a4e661a22df499eaa90ac4e3
nehasandesh <ns1193@georgetown.edu>
2022-01-31 17:54:02 -0500

    winner determination -- not finished yet

--- game.py
+++ game.py
@@ -5,3 +5,39 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+
+# ask for a user input
+
+u = input("Please choose one of: 'rock', 'paper', 'scissors?'")
+
+print("USER CHOSE:", u)
+# validations
+
+
+# comptuer choice
+
+#import random
+from random import choice 
+
+options = ["rock", "paper", "scissors"]
+computer_choice = choice(options)
+print("COMPUTER CHOSE:", computer_choice)
+
+
+# determine the winner
+#inspired from slack (credit)
+
+if u == computer_choice;
+    print("Both players chose", u, "It's a tie!")
+elif u == "paper" 
+    if computer_choice == "rock"
+
+
+# final results
+
+
+
+
+
